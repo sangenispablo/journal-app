38779229686d41a221787035cf75c522dade2152
Sangenis Pablo <sangenispablo@gmail.com>
2022-03-02 08:53:49 -0300

    Comienzo Redux

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,9 +1,12 @@
+import { Provider } from "react-redux";
+
+import { store } from "./store/store";
 import { AppRouter } from "./routers/AppRouter";
 
 export const JournalApp = () => {
   return (
-    <>
-      <AppRouter />
-    </>
+    <Provider store={store}>
+      <AppRouter />;
+    </Provider>
   );
 };
